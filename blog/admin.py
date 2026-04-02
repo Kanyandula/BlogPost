7e71fa2743996337e1dfbb699e6c94234d83118b
admin <ekanyandula8@gmail.com>
2026-04-02 20:37:40 +0100

    Add categories, tags, draft/published status, featured, and view counter
    Models:
    - Category (name, slug, description, image) with 10 Malawian-focused seeds
    - Tag (name, slug) for free-form content tagging
    - BlogPost: add category FK, tags M2M, status (draft/published),
      is_featured, view_count, reading_time property, get_related_posts()
    
    Admin:
    - Register Category/Tag with prepopulated slugs
    - BlogPost admin with list_filter, search, bulk publish/feature actions
    
    Data migration:
    - Seed 10 categories (Culture, Entertainment, Entrepreneurship, etc.)
    - Set all existing posts to status='published'

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 
-from blog.models import BlogPost
+from blog.models import BlogPost, Category, Tag
 
 
-admin.site.register(BlogPost)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug', 'created_at')
+	prepopulated_fields = {'slug': ('name',)}
 
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug')
+	prepopulated_fields = {'slug': ('name',)}
 
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'category', 'status', 'is_featured', 'view_count', 'date_updated')
+	list_filter = ('status', 'is_featured', 'category')
+	search_fields = ('title', 'body')
+	filter_horizontal = ('tags',)
+	actions = ['publish_posts', 'feature_posts']
+
+	@admin.action(description="Publish selected posts")
+	def publish_posts(self, request, queryset):
+		queryset.update(status='published')
+
+	@admin.action(description="Feature selected posts")
+	def feature_posts(self, request, queryset):
+		queryset.update(is_featured=True)
--- blog/models.py
+++ blog/models.py
@@ -1,38 +1,90 @@
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.utils.html import strip_tags
 from django.conf import settings
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from ckeditor.fields import RichTextField
+
 
 def upload_location(instance, filename):
 	file_path = 'blog/{author_id}/{title}-{filename}'.format(
-				author_id=str(instance.author.id),title=str(instance.title), filename=filename)
+				author_id=str(instance.author.id), title=str(instance.title), filename=filename)
 	return file_path
 
 
+class Category(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True)
+	description = models.TextField(max_length=500, blank=True)
+	image = models.ImageField(upload_to='categories/', null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = "categories"
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=60, unique=True)
+	slug = models.SlugField(max_length=60, unique=True)
+
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
+
 class BlogPost(models.Model):
-	title 					= models.CharField(max_length=50, null=False, blank=True)
-	#body 					= RichTextField(max_length=7000, null=False, blank=True)
-	body 					= models.TextField(max_length=7000, null=False, blank=True)
-	image		 			= models.ImageField(upload_to=upload_location, null=True, blank=True)
-	date_published 			= models.DateTimeField(auto_now_add=True, verbose_name="date published")
-	date_updated 			= models.DateTimeField(auto_now=True, verbose_name="date updated")
-	author 					= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	slug 					= models.SlugField(blank=True, unique=True)
+	STATUS_CHOICES = [
+		('draft', 'Draft'),
+		('published', 'Published'),
+	]
+
+	title = models.CharField(max_length=50, null=False, blank=True)
+	body = models.TextField(max_length=7000, null=False, blank=True)
+	image = models.ImageField(upload_to=upload_location, null=True, blank=True)
+	date_published = models.DateTimeField(auto_now_add=True, verbose_name="date published")
+	date_updated = models.DateTimeField(auto_now=True, verbose_name="date updated")
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	slug = models.SlugField(blank=True, unique=True)
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
+	tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+	is_featured = models.BooleanField(default=False)
+	view_count = models.PositiveIntegerField(default=0)
+
+	class Meta:
+		ordering = ['-date_updated']
 
 	def __str__(self):
 		return self.title
 
+	@property
+	def reading_time(self):
+		word_count = len(strip_tags(self.body).split())
+		minutes = max(1, round(word_count / 200))
+		return minutes
+
+	def get_related_posts(self, limit=4):
+		return BlogPost.objects.filter(
+			Q(category=self.category) | Q(tags__in=self.tags.all()),
+			status='published'
+		).exclude(pk=self.pk).distinct().order_by('-date_published')[:limit]
+
+
 @receiver(post_delete, sender=BlogPost)
 def submission_delete(sender, instance, **kwargs):
-    instance.image.delete(False) 
+	instance.image.delete(False)
+
 
 def pre_save_blog_post_receiver(sender, instance, *args, **kwargs):
 	if not instance.slug:
 		instance.slug = slugify(instance.author.username + "-" + instance.title)
 
 pre_save.connect(pre_save_blog_post_receiver, sender=BlogPost)
-
-
